051e5a82e817484c490327a8d4fde77b9c105a6c
Atharva Nimbalkar <g.brain2753@gmail.com>
2025-01-25 12:18:38 +0530

    error pages added

--- blogcompany/__init__.py
+++ blogcompany/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 
 from blogcompany.core.views import core
+from blogcompany.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
