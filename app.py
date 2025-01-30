d3d74bdfd13ea8809c638dc3337f5a1f7c0aea71
Atharva Nimbalkar <g.brain2753@gmail.com>
2025-01-30 12:18:02 +0530

    function created to return app instance

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from blogcompany import app
+from blogcompany import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- blogcompany/__init__.py
+++ blogcompany/__init__.py
@@ -4,30 +4,37 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app=Flask(__name__)
+# Initialize extensions without binding to an app yet
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
 
-app.config['SECRET_KEY']='mysecret'
-#db setup
-basedir=os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+def create_app():
+    app = Flask(__name__)
 
-db=SQLAlchemy(app)
-Migrate(app,db)
+    app.config['SECRET_KEY'] = 'mysecret'
 
-#login config
-login_manager=LoginManager()
+    # Database setup
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-login_manager.init_app(app)
-login_manager.login_view='users.login'#function name of the route that handles login page 
+    # Initialize extensions with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    login_manager.login_view = 'users.login'  # Function name for login route
 
+    # Import blueprints inside the function to avoid circular imports
+    from blogcompany.core.views import core
+    from blogcompany.error_pages.handlers import error_pages
+    from blogcompany.blog_posts.views import blog_posts
+    from blogcompany.users.views import users
 
-from blogcompany.core.views import core
-from blogcompany.error_pages.handlers import error_pages
-from blogcompany.blog_posts.views import blog_posts
-from blogcompany.users.views import users
+    # Register blueprints
+    app.register_blueprint(core)
+    app.register_blueprint(users)
+    app.register_blueprint(blog_posts)
+    app.register_blueprint(error_pages)
 
-app.register_blueprint(core)
-app.register_blueprint(users)
-app.register_blueprint(blog_posts)
-app.register_blueprint(error_pages)
+    return app  # Return the configured app instance
